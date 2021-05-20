9b6bdb18e36ab91dcfb706fbec7fb3985b42366d
haopeng <haopeng@bilibili.com>
2021-05-20 18:55:05 +0800

    init

---
+++ ImplicitAnimationBug/CustomControl1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace ImplicitAnimationBug
+{
+    public sealed class CustomControl1 : ContentControl
+    {
+        private Popup _popup;
+        public CustomControl1()
+        {
+            this.DefaultStyleKey = typeof(CustomControl1);
+        }
+
+        public async void Show()
+        {
+            if (_popup == null)
+            {
+                _popup = new Popup()
+                {
+                    Child = this,
+                    IsLightDismissEnabled = false,
+                };
+            }
+
+            this._popup.IsOpen = true;
+            this.Visibility = Visibility.Visible;
+            Debug.WriteLine("[toast] Visible");
+            await Task.Delay(3000);
+            Debug.WriteLine("[toast] Collapsed");
+            this._popup.IsOpen = false;
+            this.Visibility = Visibility.Collapsed;
+        }
+
+    }
+}
